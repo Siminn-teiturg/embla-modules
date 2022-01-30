7f28405f62b48e7d664da7026b093b2ea52a01f1
Logi E <logi.eyjolfsson@gmail.com>
2022-01-30 21:20:15 +0000

    modifications to grammar and output format

--- volume.py
+++ volume.py
@@ -1,40 +1,175 @@
-from typing import List, Dict, Optional, Mapping
+from typing import List, Mapping, cast
 
 import logging
-import cachetools  # type: ignore
+import json
 import random
-import math
-import re
 
 from query import Query, QueryStateDict
-from queries import query_json_api
-from tree import Result
-from reynir import NounPhrase
+from tree import Node, OptionalNode, ParamList, Result, TerminalNode
 
-from . import AnswerTuple, LatLonTuple
 
-_SIMINN_QTYPE = "Volume"
+_TV_VOLUME_QTYPE = "TV-Volume"
 
 TOPIC_LEMMAS = [
     "hækka",
+    "hækkaðu",
     "lækka",
+    "lækkaðu",
     "þögn",
     "þagna",
     "hljóð",
     "hljóðstyrkur",
 ]
 
+
+def help_text(lemma: str) -> str:
+    """Help text to return when query.py is unable to parse a query but
+    one of the above lemmas is found in it"""
+    return "Ég get svarað ef þú spyrð til dæmis: {0}?".format(
+        random.choice(
+            (
+                "Hækka hljóðið",
+                "Lækka hljóðstyrkinn",
+                "Hækka",
+                "Þagna",
+                "Þögn",
+                "Hljóð fimmtíu prósent",
+            )
+        )
+    )
+
+
+# This module wants to handle parse trees for queries
+HANDLE_TREE = True
+
+# The grammar nonterminals this module wants to handle
+QUERY_NONTERMINALS = {"QTVVolume"}
+
+# The context-free grammar for the queries recognized by this plug-in module
+GRAMMAR = """
+
+Query → QTVVolume
+
+QTVVolume → QTVVolumeQuery '?'?
+
+QTVVolumeQuery →
+    QTVVolumeRelative
+    | QTVVolumeAbsolute
+
+# "hækkaðu um tuttugu prósent", "lækkaðu um fimmtíu og einn"
+QTVVolumeRelative →
+    QTVVolumeIncrease QTVVolumeHljóð? QTVVolumeUmAmount?
+    | QTVVolumeDecrease QTVVolumeHljóð? QTVVolumeUmAmount?
+
+QTVVolumeUmAmount → "um"? QTVVolumeAmount
+
+# "stilltu hljóðið í fimmtíu prósent", "settu hljóðið í tuttugu"
+QTVVolumeAbsolute →
+    QTVVolumeSet? QTVVolumeHljóð "í"? QTVVolumeAmount
+    | "hækkaðu" "í" QTVVolumeAmount
+    | "lækkaðu" "í" QTVVolumeAmount
+
+QTVVolumeIncrease →
+    "hækka" "þú"?
+    | "hækkaðu"
+    | "hækkað"
+    # | "fækka"
+
+QTVVolumeDecrease →
+    "lækka" "þú"?
+    | "lækkaðu"
+    | "lækkað"
+    | "lægra"
+    | "lægri"
+    | "minnka" "þú"?
+    | "minnkaðu"
+
+QTVVolumeSet →
+    "stilltu"
+    | "stilla"
+    | "setja"
+    | "settu"
+
+
+QTVVolumeHljóð →
+    "hljóð"
+    | "hljóðið"
+    | "hljóðstyrk"
+    | "hljóðstyrkinn"
+
+QTVVolumeAmount →
+    QTVVolumePercent
+    | QTVVolumeNumber "prósent"?
+
+QTVVolumePercent →
+    Prósenta
+    | QTVVolumeNumTens "og" Prósenta
+
+
+QTVVolumeNumber →
+    QTVVolumeNum0
+    | QTVVolumeNum1To9
+    | QTVVolumeNum10To19
+    | QTVVolumeNumTens
+    | QTVVolumeNumTens "og" QTVVolumeNum1To9
+    | QTVVolumeNum100
+
+QTVVolumeNum0 → "núll"
+
+QTVVolumeNum1To9 →
+    'einn:to'
+    | 'tveir:to'
+    | 'þrír:to'
+    | 'fjórir:to'
+    | "fimm"
+    | "sex"
+    | "sjö"
+    | "átta"
+    | "níu"
+
+QTVVolumeNum10To19 →
+    "tíu"
+    | "ellefu"
+    | "tólf"
+    | "þrettán"
+    | "fjórtán"
+    | "fimmtán"
+    | "sextán"
+    | "sautján"
+    | "átján"
+    | "nítján"
+
+QTVVolumeNumTens →
+    "tuttugu"
+    | "þrjátíu"
+    | "fjörutíu"
+    | "fjörtíu"
+    | "fimmtíu"
+    | "sextíu"
+    | "sjötíu"
+    | "áttatíu"
+    | "níutíu"
+
+QTVVolumeNum100 →
+    "eitt"? "hundrað"
+
+"""
+
 _NUMBER_WORDS: Mapping[str, float] = {
     "núll": 0,
     "einn": 1,
     "einu": 1,
+    "eitt": 1,
     "tveir": 2,
     "tveim": 2,
+    "tvö": 2,
     "þrír": 3,
     "þrem": 3,
+    "þrjú": 3,
     "þremur": 3,
     "fjórir": 4,
     "fjórum": 4,
+    "fjögur": 4,
     "fimm": 5,
     "sex": 6,
     "sjö": 7,
@@ -53,6 +188,7 @@ _NUMBER_WORDS: Mapping[str, float] = {
     "tuttugu": 20,
     "þrjátíu": 30,
     "fjörutíu": 40,
+    "fjörtíu": 40,
     "fimmtíu": 50,
     "sextíu": 60,
     "sjötíu": 70,
@@ -62,130 +198,115 @@ _NUMBER_WORDS: Mapping[str, float] = {
     "hundruð": 100,
 }
 
-def help_text(lemma: str) -> str:
-    """Help text to return when query.py is unable to parse a query but
-    one of the above lemmas is found in it"""
-    return "Ég get svarað ef þú spyrð til dæmis: {0}?".format(
-        random.choice(
-            (
-                "Hækka hljóðið",
-                "Lækka hljóðstyrkinn",
-                "Hækka",
-                "Þagna",
-                "Þögn!",
-                "hljóð fimmtíu prósent"
-            )
-        )
-    )
 
-# This module wants to handle parse trees for queries
-HANDLE_TREE = True
+def QTVVolumeQuery(node: Node, params: ParamList, result: Result) -> None:
+    result.qtype = _TV_VOLUME_QTYPE
 
-# The grammar nonterminals this module wants to handle
-QUERY_NONTERMINALS = {"QVolume"}
 
-# The context-free grammar for the queries recognized by this plug-in module
-GRAMMAR = """
+def Prósenta(node: Node, params: ParamList, result: Result) -> None:
+    tnode: OptionalNode = node.first_child(lambda x: x.has_t_base("prósenta"))
+    if tnode:
+        tnode = cast(TerminalNode, tnode)
+        # Extract number value from percentage terminal node
+        result["numbers"] = [json.loads(tnode.aux)[0]]
 
-Query →
-    QVolume
- 
-QVolume → QVolumeQuery '?'?
 
-QVolumeQuery →
-    QVolumeUpQuery QVolumeVolume?
-    | QVolumeDownQuery QVolumeVolume?
-    | QVolumeVolume QVolumePercent
+def QTVVolumeNum0(node: Node, params: ParamList, result: Result) -> None:
+    result["numbers"] = [0]
 
-QVolumeUpQuery →
-    "hækka" | "hækka" "þú" | "hækkaðu" | "hækkað" 
-    | "fækka"
 
-QVolumeDownQuery →
-    "lækka" | "lækkað" | "lækka" "þú" | "lækkaðu" 
+def QTVVolumeNum1To9(node: Node, params: ParamList, result: Result) -> None:
+    r = _NUMBER_WORDS.get(result._root)
+    if r:
+        result["numbers"] = [r]
 
-QVolumeVolume →
-    "hljóð" | "hljóðið" | "hljóðstyrk" | "hljóðstyrkinn"
 
-QVolumePercent → Prósenta
+def QTVVolumeNum10To19(node: Node, params: ParamList, result: Result) -> None:
+    r = _NUMBER_WORDS.get(result._root)
+    if r:
+        result["numbers"] = [r]
 
-"""
 
-def QVolumePercent(node, params, result):
-    result._canonical = result._text
-    n = result._text.split()
-    print(result._canonical)
-    print("N:", n, type(n))
-    # skoda rett control flow
-    # hvad gaeti fokkad thessu upp?
-    if n[0].isdecimal():
-        print(n[0], "is decimal")
-        result["command"] = int(n[0])           # 8
-    elif n[0] in _NUMBER_WORDS:
-        print(n[0], "is not decimal")
-        result["command"] = _NUMBER_WORDS[n[0]] # átta
+def QTVVolumeNumTens(node: Node, params: ParamList, result: Result) -> None:
+    r = _NUMBER_WORDS.get(result._root)
+    if r:
+        result["numbers"] = [r]
+
+
+def QTVVolumeNum100(node: Node, params: ParamList, result: Result) -> None:
+    result["numbers"] = [100]
+
+
+def QTVVolumeIncrease(node: Node, params: ParamList, result: Result) -> None:
+    result["command"] = ["VOLUME_REL"]
+
+
+def QTVVolumeDecrease(node: Node, params: ParamList, result: Result) -> None:
+    result["command"] = ["VOLUME_REL"]
+    result["negative"] = True
+
+
+def QTVVolumeAbsolute(node: Node, params: ParamList, result: Result) -> None:
+    result["command"] = ["VOLUME_ABS"]
+
+
+def _parse_numbers(result: Result) -> int:
+    """
+    Parse numbers in query/command and
+    combine into a single number.
+    """
+    nums: List[int] = result.get("numbers", [])
+
+    if len(nums) == 0:
+        # By default the volume increment is just 1
+        return 1
+
+    if len(nums) == 1:
+        final_num = nums[0]
+    elif len(nums) >= 2:
+        n1, n2 = nums
+
+        if 19 < n1 < 100 and n1 % 10 == 0 and 0 < n2 < 10:
+            # Normal, e.g. "fimmtíu og eitt prósent"
+            final_num = n1 + n2
+        else:
+            # Weird queries, e.g. "tuttugu og þrjátíu prósent"
+            # Just ignore first number and take second one
+            final_num = n2
     else:
-        print("tjah, hvað er í gangi hér?")
+        return 1
 
-def QVolumeQuery(node, params, result):
-    result.qtype = _SIMINN_QTYPE
+    # Keep number in range 0-100
+    if final_num < 0:
+        final_num = 0
+    elif final_num > 100:
+        final_num = 100
 
+    if "negative" in result and result["negative"]:
+        # Lowering the volume
+        final_num = -final_num
 
-def QVolumeUpQuery(node, params, result):
-    result["command"] = "VOLUME_UP"
+    return int(final_num)
 
-
-def QVolumeDownQuery(node, params, result):
-    result["command"] = "VOLUME_DOWN"
-
-
-def QVolumeMuteQuery(node, params, result):
-    result["command"] = "MUTE"
-
-
-def parse_num(num_str: str) -> float:
-    # Parse Icelandic number string to float or int
-    num = None
-    try:
-        # Pi
-        if num_str == "pí":
-            num = math.pi
-        # Handle numbers w. Icelandic decimal places ("17,2")
-        elif re.search(r"^\d+,\d+$", num_str):
-            num = float(num_str.replace(",", "."))
-        # Handle digits ("17")
-        else:
-            num = float(num_str)
-    except ValueError:
-        # Handle number words ("sautján")
-        if num_str in _NUMBER_WORDS:
-            num = _NUMBER_WORDS[num_str]
-        # Ordinal number strings ("17.")
-        elif re.search(r"^\d+\.$", num_str):
-            num = int(num_str[:-1])
-        else:
-            num = 0
-    except Exception as e:
-        logging.warning("Unexpected exception: {0}".format(e))
-        raise
-    return num
 
 def sentence(state: QueryStateDict, result: Result) -> None:
     """Called when sentence processing is complete."""
     q: Query = state["query"]
-    if (
-        "qtype" in result
-        and result["qtype"] == _SIMINN_QTYPE
-    ):
+    if "qtype" in result and result["qtype"] == _TV_VOLUME_QTYPE:
         try:
-            print("))============>", _SIMINN_QTYPE, "<============((")
-            q.set_qtype(_SIMINN_QTYPE)
-            q.set_answer("", result["command"], "")
+            q.query_is_command()
+            q.set_qtype(_TV_VOLUME_QTYPE)
+
+            cmd_list: List[str] = result["command"]
+            cmd_list.append(str(_parse_numbers(result)))
+
+            # Return the command as a string delimited by semicolons
+            # Format: (VOLUME_REL|VOLUME_ABS);<value>
+            ans = ";".join(cmd_list)
+            q.set_answer({"answer": ans}, ans)
             return
         except Exception as e:
-            logging.warning(
-                "Exception generating answer from Volume: {0}".format(e)
-            )
+            logging.warning("Exception generating answer from Volume: {0}".format(e))
             q.set_error("E_EXCEPTION: {0}".format(e))
     else:
         q.set_error("E_QUERY_NOT_UNDERSTOOD")
